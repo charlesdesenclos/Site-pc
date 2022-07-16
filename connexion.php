638d8b763020a01563f4eb58fd081f78135b76d5
charlesdesenclos <75635265+charlesdesenclos@users.noreply.github.com>
2022-07-16 11:08:19 +0200

    commit 1.2

--- connexion.php
+++ connexion.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
+        <title>Connexion</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="main.css">
@@ -23,6 +23,18 @@
 
         
     ?>
+
+
+<header>
+    <nav>
+        <ul class="nav_links">
+            <li><a href="index.php"> Accueil</a></li>
+            <li><a href="liste_pc.php"> Nos produits</a></li>
+            <li><a href="connexion.php"> Connexion</a></li>
+        </ul>
+            
+    </nav>
+</header>
 
 
 <div id="container">
