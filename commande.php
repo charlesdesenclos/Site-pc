9c6f8fe4b1ae5a1f09917cc62d94d6393723192c
charlesdesenclos <75635265+charlesdesenclos@users.noreply.github.com>
2022-08-01 10:06:52 +0200

    commit 1.6.8.4

--- commande.php
+++ commande.php
@@ -11,7 +11,30 @@
         <link rel="stylesheet" href="main.css">
     </head>
     <body>
-      
+    <?php
+        require_once 'pdo/config.php'; // ajout connexion bdd 
+    ?>
+
+
+<header>
+    <nav>
+        <ul class="nav_links">
+            <li><a href="index.php"> Accueil</a></li>
+            <li><a href="liste_pc.php"> Nos produits</a></li>
+            <li><a href="connexion.php"> Connexion</a></li>
+            <?php 
+                    if(isset($_SESSION['Connexion']))  //affiche la déconnexion,la liste des commandes, la page pour modifier et supprimer une commande quand l'utilisateur est connecté
+                     {?>
+                        <li><a href="deconnexion.php">Déconnexion</a></li>
+                        <li><a href="liste_commande.php">Liste des commandes</a></li>
+                        <li><a href="modification_pc.php">Modifier votre commande</a></li>
+                        <li><a href="suppresion_pc.php">Supprimer votre commande</a></li><?php
+                    }
+            ?>
+        </ul>
+            
+    </nav>
+</header>
         
         <script src="" async defer></script>
     </body>
