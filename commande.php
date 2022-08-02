d635ab50d990d561c4ad371ac47629bdd2cd8018
charlesdesenclos <75635265+charlesdesenclos@users.noreply.github.com>
2022-08-02 10:37:53 +0200

    commit 1.6.10

--- commande.php
+++ commande.php
@@ -40,6 +40,9 @@
 </header>
 
 <?php
+    $resultEtape1 = false;
+    $resultEtape2 = false;
+    
     if(isset($_POST['Adresse']))
     {
         $num_porte = $_POST['num_porte'];
@@ -49,6 +52,21 @@
 
         $reqAdresse = "INSERT INTO adresse (numero_porte, rue, ville, code_postale) VALUES ( '".$num_porte."' , '".$rue."', '".$ville."', '".$code_postale."')";
         $resultat = $GLOBALS['bdd'] -> query($reqAdresse);
+
+        $resultEtape1 = true;
+    }
+
+    if(isset($_POST['Bancaire']))
+    {
+        $nom_carte = $_POST['nom_carte'];
+        $num_carte = $_POST['num_carte'];
+        $date_expiration = $_POST['date_expiration'];
+        $cvc = $_POST['cvc'];
+
+        $reqBancaire = "INSERT INTO bancaire (nom_carte, numero_carte, date_expiration, cvc) VALUES ( '".$nom_carte."' , '".$num_carte."', '".$date_expiration."', '".$cvc."')";
+        $resultat = $GLOBALS['bdd'] -> query($reqBancaire);
+
+        $resultEtape2 = true;
     }
 
 
@@ -56,7 +74,10 @@
 
 <div id="container">
 
-
+    <?php
+            if($resultEtape1 == false && $resultEtape2 == false)
+            {?>
+            <!--Adresse-->
                 <form action="" method="POST" >
                     <h1>Etapes 1:</h1>
                     
@@ -71,11 +92,70 @@
                     <input type="text" placeholder="Entrez votre code postale" name="code_postale" required>
 
                     
-                    <input type="submit"  id="submit" value="Adresse" name="Adresse">
+                    <input type="submit"  id="submit" value="Valider votre Adresse" name="Adresse">
 
                     
                     
                 </form>
+
+            <?php
+            }
+            else if($resultEtape1 == true && $resultEtape2 == false)
+            {?>
+            <!--Coordonnées Bancaires-->
+                <form action="" method="POST" >
+                    <h1>Etapes 2:</h1>
+                    
+                    <h2>Coordonnées Bancaires</h2>
+
+                    <input type="text" placeholder="Entrez votre nom de carte" name="nom_carte" required>
+
+                    <input type="text" placeholder="Entrez votre numéro de carte" name="num_carte" required>
+
+                    <input type="text" placeholder="Entrez la date d'expiration" name="date_expiration" required>
+
+                    <input type="text" placeholder="Entrez votre cvc" name="cvc" required>
+
+                    
+                    <input type="submit"  id="submit" value="Valider vos coordonnées bancaires" name="Bancaire">
+
+                    
+                    
+                </form>
+            <?php    
+            }
+            else if($resultEtape1 == true && $resultEtape2 == true)
+            {?>
+            <!--Commande-->
+                <form action="" method="POST" >
+                    <h1>Etapes 3:</h1>
+                    
+                    <h2>Commande</h2>
+
+                    <label><b>PC :</b></label> 
+                    <select name="id_pc" id="select-pc">
+                        <option value="">Choisisez votre pc</option>
+                        <option value="1">PC HAUT DE GAMME</option>
+                        <option value="2">PC MEDIUM</option>
+                        <option value="3">PC PORTABLE HAUT DE GAMME</option>
+                        <option value="4">PC PORTABLE MEDIUM</option>
+                    </select>
+
+                    
+                    <input type="submit"  id="submit" value="Commander" name="Commande">
+
+                    
+                    
+                </form>
+                <?php
+            }
+
+
+
+
+
+
+    ?>
             <!--commande-->
             <!--
                 <form action="" method="POST" >
