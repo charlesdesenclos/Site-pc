9513519d8481f521e4c1f62a9cb5f339b2ee9546
charlesdesenclos <75635265+charlesdesenclos@users.noreply.github.com>
2022-08-15 11:08:21 +0200

    commit 1.8.3

--- suppresion_pc.php
+++ suppresion_pc.php
@@ -14,7 +14,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
+        <title>Supprimer Votre Commande</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="icon" type="image/png" sizes="16x16" href="Image/logo_pc.jpg">
