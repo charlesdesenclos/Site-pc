3f58e9135585ce0ec23df2a428184729e6e1b0a3
charlesdesenclos <75635265+charlesdesenclos@users.noreply.github.com>
2022-08-27 20:36:56 +0200

    commit 1.8.5

--- Classe/User.php
+++ Classe/User.php
@@ -56,7 +56,7 @@
         public function deconnexion()
         {
             session_start(); // demarrage de la session
-            session_destroy(); // on détruit la/les session(s), soit si vous utilisez une autre session, utilisez de préférence le unset()
+            session_destroy(); // on détruit la/les session(s), soit si vous utilisez une autre session
             header('Location: index.php'); // On redirige
             die();
         }
